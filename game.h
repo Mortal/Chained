b81d62e53c1232625c4240b5b4b3e8376e8761dd
Mathias Rav <rav@cs.au.dk>
2011-12-23 17:46:55 +0100

    Move Game::key into game.h

--- game.h
+++ game.h
@@ -1,5 +1,6 @@
 /* vim:set ts=4 sw=4 sts=4 noet: */
 #include <GL/gl.h>
+#include "root.h"
 
 class Game {
 	public:
@@ -9,7 +10,39 @@ class Game {
 		int points;
 		class ScoringSystem ss;
 		void draw();
-		void key(int which, byte dir);
+		inline void key(int which, byte dir) {
+			if DEBUGIT_KEYS logfile << "Game::key	  Game " << which << " direction " << (int) dir << std::endl;
+			switch (which) {
+				case GAMEKEY_LEFT:
+					if (dir == KEYTYPE_UP) break;
+					if (this->cursorx > 0) this->cursorx--;
+					break;
+				case GAMEKEY_UP:
+					if (dir == KEYTYPE_UP) break;
+					if (this->cursory > 0) this->cursory--;
+					break;
+				case GAMEKEY_RIGHT:
+					if (dir == KEYTYPE_UP) break;
+					if ((this->cursorx+1) < this->field.getwidth()-1) this->cursorx++;
+					break;
+				case GAMEKEY_DOWN:
+					if (dir == KEYTYPE_UP) break;
+					if ((this->cursory) < this->field.getheight()-1) this->cursory++;
+					break;
+				case GAMEKEY_SWITCH:
+					if (dir != KEYTYPE_DOWN) break;
+					this->field.swap(this->cursorx, this->cursory, this->cursorx+1, this->cursory);
+					break;
+				case GAMEKEY_STACK:
+					if (dir == KEYTYPE_DOWN) {
+						this->field.stoptime = 0;
+						this->field.speedrising_key = 1;
+					} else if (dir == KEYTYPE_UP) {
+						this->field.speedrising_key = 0;
+					}
+					break;
+			}
+}
 		Game() {cursorx = 2; cursory = 4;}
 		void drawbackground(GLfloat offx, GLfloat offy);
 		void drawcursors(GLfloat offx, GLfloat offy);
--- main.cpp
+++ main.cpp
@@ -204,40 +204,6 @@ void Key(int key, byte dir) {
 	}
 }
 
-void Game::key(int which, byte dir) {
-	if DEBUGIT_KEYS logfile << "Game::key	  Game " << which << " direction " << (int) dir << std::endl;
-	switch (which) {
-		case GAMEKEY_LEFT:
-			if (dir == KEYTYPE_UP) break;
-			if (this->cursorx > 0) this->cursorx--;
-		break;
-		case GAMEKEY_UP:
-			if (dir == KEYTYPE_UP) break;
-			if (this->cursory > 0) this->cursory--;
-		break;
-		case GAMEKEY_RIGHT:
-			if (dir == KEYTYPE_UP) break;
-			if ((this->cursorx+1) < this->field.getwidth()-1) this->cursorx++;
-		break;
-		case GAMEKEY_DOWN:
-			if (dir == KEYTYPE_UP) break;
-			if ((this->cursory) < this->field.getheight()-1) this->cursory++;
-		break;
-		case GAMEKEY_SWITCH:
-			if (dir != KEYTYPE_DOWN) break;
-			this->field.swap(this->cursorx, this->cursory, this->cursorx+1, this->cursory);
-		break;
-		case GAMEKEY_STACK:
-			if (dir == KEYTYPE_DOWN) {
-				this->field.stoptime = 0;
-				this->field.speedrising_key = 1;
-			} else if (dir == KEYTYPE_UP) {
-				this->field.speedrising_key = 0;
-			}
-		break;
-	}
-}
-
 int tick() {
 	if (paused) {
 		if (!goframe) return 1;
