2149650b6f9b40849679b08f648c26c1d01ccaf0
Mathias Rav <rav@cs.au.dk>
2011-12-23 19:23:09 +0100

    Make vkeys.h use consts rather than defines

--- vkeys.h
+++ vkeys.h
@@ -1,20 +1,23 @@
 /* vim:set ts=4 sw=4 sts=4 noet: */
-#define VK_OEM_COMMA	0xBC
-#define VK_OEM_PERIOD   0xBE
-#define VK_F1		   0x70
-#define VK_F2		   0x71
-#define VK_F3		   0x72
-#define VK_F4		   0x73
-#define VK_F5		   0x74
-#define VK_F6		   0x75
-#define VK_F7		   0x76
-#define VK_F8		   0x77
-#define VK_F9		   0x78
-#define VK_F10		  0x79
-#define VK_F11		  0x7A
-#define VK_F12		  0x7B
-#define VK_HOME		 36
-#define VK_LEFT		 37
-#define VK_UP		   38
-#define VK_RIGHT		39
-#define VK_DOWN		 40
+#ifndef CHAINED_VKEYS
+#define CHAINED_VKEYS
+const int VK_OEM_COMMA = 0xBC;
+const int VK_OEM_PERIOD = 0xBE;
+const int VK_F1 = 0x70;
+const int VK_F2 = 0x71;
+const int VK_F3 = 0x72;
+const int VK_F4 = 0x73;
+const int VK_F5 = 0x74;
+const int VK_F6 = 0x75;
+const int VK_F7 = 0x76;
+const int VK_F8 = 0x77;
+const int VK_F9 = 0x78;
+const int VK_F10 = 0x79;
+const int VK_F11 = 0x7A;
+const int VK_F12 = 0x7B;
+const int VK_HOME = 36;
+const int VK_LEFT = 37;
+const int VK_UP = 38;
+const int VK_RIGHT = 39;
+const int VK_DOWN = 40;
+#endif
