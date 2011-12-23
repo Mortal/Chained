1bbc080679f14c2e1c0ed538d64bf629a719bae5
Mathias Rav <rav@cs.au.dk>
2011-12-23 18:03:27 +0100

    Move Game::drawcursors into game.h

--- game.h
+++ game.h
@@ -78,7 +78,68 @@ class Game {
 			if DEBUGIT_DRAWBACKGROUND logfile << "Game::drawbg.   " << this->id << " " << n*SCREEN_HEIGHT << " " << s*SCREEN_HEIGHT << " " << w*SCREEN_WIDTH << " " << e*SCREEN_WIDTH << std::endl;
 			if DEBUGIT_DRAWBACKGROUND logfile << "Game::drawbg.   " << this->id << " " << (n-s)*SCREEN_HEIGHT << " " << (e-w)*SCREEN_WIDTH << std::endl;
 		}
-		void drawcursors(GLfloat offx, GLfloat offy);
+		void drawcursors(GLfloat offx, GLfloat offy) {
+			offx += (GLfloat) 2 * (this->cursorx * BLOCK_WIDTH) / (GLfloat) SCREEN_WIDTH;
+			offy += (GLfloat) 2 * ((this->field.getheight()-this->cursory-1) * BLOCK_HEIGHT) / (GLfloat) SCREEN_HEIGHT;
+			GLfloat topn = offy + (CURSOR_MARGIN / (GLfloat) SCREEN_HEIGHT);
+			GLfloat tops = offy - ((CURSOR_PADDING) / (GLfloat) SCREEN_HEIGHT);
+			offy += (GLfloat) 2 * (BLOCK_HEIGHT) / (GLfloat) SCREEN_HEIGHT;
+			GLfloat bottomn = offy + 2 * (CURSOR_PADDING / (GLfloat) SCREEN_HEIGHT);
+			GLfloat bottoms = offy - 2 * ((CURSOR_MARGIN) / (GLfloat) SCREEN_HEIGHT);
+			GLfloat leftw = offx + 2 * (CURSOR_MARGIN / (GLfloat) SCREEN_WIDTH);
+			GLfloat lefte = offx - 2 * ((CURSOR_PADDING) / (GLfloat) SCREEN_WIDTH);
+			offx += (GLfloat) 2 * (BLOCK_WIDTH) / (GLfloat) SCREEN_WIDTH;
+			GLfloat midw = offx + 2 * (CURSOR_PADDING / (GLfloat) SCREEN_WIDTH);
+			GLfloat mide = offx - 2 * ((CURSOR_PADDING) / (GLfloat) SCREEN_WIDTH);
+			offx += (GLfloat) 2 * (BLOCK_WIDTH) / (GLfloat) SCREEN_WIDTH;
+			GLfloat rightw = offx + 2 * (CURSOR_PADDING / (GLfloat) SCREEN_WIDTH);
+			GLfloat righte = offx - 2 * ((CURSOR_MARGIN) / (GLfloat) SCREEN_WIDTH);
+			if DEBUGIT_GAMEDRAW logfile << "Game::drawcurs. " << offy << " " << topn << " " << tops << " " << bottomn <<
+				" " << bottoms << "  " << offx << " " << leftw << " " << lefte << " " << 
+					midw << " " << mide << " " << rightw << " " << righte << std::endl;
+
+			// Top bar
+			glBindTexture (GL_TEXTURE_2D, 0);
+			glBegin(GL_QUADS);
+			glColor4f(1.0f, 1.0f, 1.0f, 0.5f);
+			glVertex2f(leftw, topn);
+			glVertex2f(leftw, tops);
+			glVertex2f(righte, tops);
+			glVertex2f(righte, topn);
+			glEnd();
+			// Bottom bar
+			glBegin(GL_QUADS);
+			glColor4f(1.0f, 1.0f, 1.0f, 0.5f);
+			glVertex2f(leftw, bottomn);
+			glVertex2f(leftw, bottoms);
+			glVertex2f(righte, bottoms);
+			glVertex2f(righte, bottomn);
+			glEnd();
+			// Left bar
+			glBegin(GL_QUADS);
+			glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+			glVertex2f(leftw, tops);
+			glVertex2f(lefte, tops);
+			glVertex2f(lefte, bottomn);
+			glVertex2f(leftw, bottomn);
+			glEnd();
+			// Mid bar
+			glBegin(GL_QUADS);
+			glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+			glVertex2f(midw, tops);
+			glVertex2f(mide, tops);
+			glVertex2f(mide, bottomn);
+			glVertex2f(midw, bottomn);
+			glEnd();
+			// Right bar
+			glBegin(GL_QUADS);
+			glColor4f(1.0f, 1.0f, 1.0f, 1.0f);
+			glVertex2f(rightw, tops);
+			glVertex2f(righte, tops);
+			glVertex2f(righte, bottomn);
+			glVertex2f(rightw, bottomn);
+			glEnd();
+		}
 		//class Root *parent; will always be static
 		int id;
 };
