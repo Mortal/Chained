4acf74bc45923c376bd723c297f4a2771203e2ea
Mathias Rav <rav@cs.au.dk>
2011-12-23 18:29:39 +0100

    Move SwapStack::swap into swapstack.h

--- main.cpp
+++ main.cpp
@@ -293,40 +293,6 @@ void draw() {
 	}
 }
 
-void SwapStack::swap(Block *block, int fromx, int fromy, int tox, int toy, byte switchblock, Field *parent) {
-	if (!block) {
-		if DEBUGIT_SWAPSTACKSWAP logfile << "SwapStack::swap Oh my, nullpointered block." << std::endl;
-		return;
-	}
-	if (!block->swapable()) {
-		if DEBUGIT_SWAPSTACKSWAP logfile << "SwapStack::swap Oh my, block not swap(p)able." << std::endl;
-		return;
-	}
-	block->state = BLOCKSTATE_SWITCHING;
-	if (DEBUGIT_SWAPSTACKSWAP || DEBUGIT_SWAPSTACKSWAP_MEMORY) logfile << "SwapStack::swap Starting." << std::endl;
-	int idx = -1;
-	for (int i = 0; i < stacklen; i++) {
-		if (stack[i]) continue;
-		if DEBUGIT_SWAPSTACKSWAP_MEMORY logfile << "SwapStack::swap -- We got an unused swapstack index, " << i << ". We'll use that." << std::endl;
-		idx = i;
-		break;
-	}
-	if (idx < 0) {
-		if (stacklen >= MAXSWAPS) {
-			if DEBUGIT_SWAPSTACKSWAP_MEMORY logfile << "SwapStack::swap canceled! oh my, we ran out of swaps." << std::endl;
-			return;
-		}
-		stacklen++;
-		idx = stacklen-1;
-	}
-	if (idx < 0) {
-		if DEBUGIT_SWAPSTACKSWAP_MEMORY logfile << "SwapStack::swap -- Hmm! idx is still lower than 0 even though we went through making a new stack and all. " << idx << std::endl;
-	}
-	if DEBUGIT_SWAPSTACKSWAP_MEMORY logfile << "SwapStack::swap -- stack[idx = " << idx << "] = new Swap(block = " << block << ", fromx,y,tox,y = " << fromx << "," << fromy << "," << tox << "," << toy << ")" << std::endl;
-	stack[idx] = new Swap(block, fromx, fromy, tox, toy, switchblock, parent);
-	if (DEBUGIT_SWAPSTACKSWAP || DEBUGIT_SWAPSTACKSWAP_MEMORY) logfile << "SwapStack::swap ending." << std::endl;
-}
-
 Swap::Swap(Block *block_, int fromx_, int fromy_, int tox_, int toy_, byte switchblock_, class Field *parentfield_) {
 	this->block = block_;
 	this->fromx = fromx_;
--- swapstack.h
+++ swapstack.h
@@ -13,7 +13,39 @@ class SwapStack {
 				}
 			}
 		}
-		void swap(Block *block, int fromx, int fromy, int tox, int toy, byte switchblock, class Field *parent);
+		void swap(Block *block, int fromx, int fromy, int tox, int toy, byte switchblock, class Field *parent) {
+			if (!block) {
+				if DEBUGIT_SWAPSTACKSWAP logfile << "SwapStack::swap Oh my, nullpointered block." << std::endl;
+				return;
+			}
+			if (!block->swapable()) {
+				if DEBUGIT_SWAPSTACKSWAP logfile << "SwapStack::swap Oh my, block not swap(p)able." << std::endl;
+				return;
+			}
+			block->state = BLOCKSTATE_SWITCHING;
+			if (DEBUGIT_SWAPSTACKSWAP || DEBUGIT_SWAPSTACKSWAP_MEMORY) logfile << "SwapStack::swap Starting." << std::endl;
+			int idx = -1;
+			for (int i = 0; i < stacklen; i++) {
+				if (stack[i]) continue;
+				if DEBUGIT_SWAPSTACKSWAP_MEMORY logfile << "SwapStack::swap -- We got an unused swapstack index, " << i << ". We'll use that." << std::endl;
+				idx = i;
+				break;
+			}
+			if (idx < 0) {
+				if (stacklen >= MAXSWAPS) {
+					if DEBUGIT_SWAPSTACKSWAP_MEMORY logfile << "SwapStack::swap canceled! oh my, we ran out of swaps." << std::endl;
+					return;
+				}
+				stacklen++;
+				idx = stacklen-1;
+			}
+			if (idx < 0) {
+				if DEBUGIT_SWAPSTACKSWAP_MEMORY logfile << "SwapStack::swap -- Hmm! idx is still lower than 0 even though we went through making a new stack and all. " << idx << std::endl;
+			}
+			if DEBUGIT_SWAPSTACKSWAP_MEMORY logfile << "SwapStack::swap -- stack[idx = " << idx << "] = new Swap(block = " << block << ", fromx,y,tox,y = " << fromx << "," << fromy << "," << tox << "," << toy << ")" << std::endl;
+			stack[idx] = new Swap(block, fromx, fromy, tox, toy, switchblock, parent);
+			if (DEBUGIT_SWAPSTACKSWAP || DEBUGIT_SWAPSTACKSWAP_MEMORY) logfile << "SwapStack::swap ending." << std::endl;
+		}
 		SwapStack() {this->stacklen = 0;}
 		inline int swapCount() {
 			int count = 0;
